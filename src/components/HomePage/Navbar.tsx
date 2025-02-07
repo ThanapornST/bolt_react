4f2102c7ce1aa0e890cdcf03c2f4cb9e78ba679c
thanaporn029 <s64122250029@ssru.ac.th>
2025-02-07 12:07:24 +0700

    re

--- src/components/HomePage/Navbar.tsx
+++ src/components/HomePage/Navbar.tsx
@@ -24,11 +24,11 @@ const Navbar = () => {
           <Link to="/create" className="text-white hover:text-blue-400 transition-colors">Write a Story</Link>
           <a href="#" className="text-white hover:text-blue-400 transition-colors">Showcase</a>
           <a href="#" className="text-white hover:text-blue-400 transition-colors">Blogs</a>
-          <a href="#" className="text-white hover:text-blue-400 transition-colors">Pricing</a>
+          <Link to="/pricing" className="text-white hover:text-blue-400 transition-colors">Pricing</Link>
           <a href="#" className="text-white hover:text-blue-400 transition-colors">About Us</a>
         </div>
         <div className="flex gap-4">
-          <Link to="/register" className="px-4 py-2 text-white hover:text-blue-300 transition-colors">
+        <Link to="/pricing" className="px-4 py-2 text-white hover:text-blue-300 transition-colors">
             Try for free
           </Link>
           <Link to="/login" className="px-4 py-2 text-white hover:text-blue-300 transition-colors">
